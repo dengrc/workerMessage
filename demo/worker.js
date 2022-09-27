f333e0d4e81f68959bf14779e09c3760ceb499c4
dengruchun <1107151332@qq.com>
2022-09-27 17:44:55 +0800

    test

--- demo/worker.js
+++ demo/worker.js
@@ -6,6 +6,12 @@ message.onRequestMessage = function (message, resolve, reject) {
     resolve(message + "; worker response")
 }
 
+message.onMessage = function (message) {
+    resolve(message)
+}
+
+message.postMessage("main")
+
 message.requestMessage("to main").then((e) => {
     console.log(e)
 }).catch((e) => {
